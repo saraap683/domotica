30e4da5241ef049cee8212e0b0e0a7b4379372b0
saraap683 <saraaguado@iesteis.net>
2025-05-20 14:18:10 +0200

    Update SaraAguadoP4.py

--- programas/SaraAguadoP4.py
+++ programas/SaraAguadoP4.py
@@ -1,12 +1,14 @@
- from microbit import*
+pin2.set_analog_period(20) # Servo coenctado al pin 2
+pin2.write_analog(1) #La puerta comienza cerrada
+porta = 0 
  
- servo1 = pin2
- angulos = 0
- 
- while True :
-     if butto_b.is_pressed():
-         angulo +=10
-         servo.write_analog(angulo)
-         if angulo >= 90:
-             angulo = 0
-             servo write_analog(angulo)
+while True :
+    if button_b.is_pressed(): #Si el botón b esta presionado
+        if porta == 0:
+            pin2.write_analog(90) #Puerta abierta a 90º
+            porta=1
+        else:
+            pin2.write_analog(1) 
+            porta=0
+
+    sleep(100)
